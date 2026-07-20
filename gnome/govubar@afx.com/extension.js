8718055fd6ead295b04c2ad0555dcbf2fa9e90e7
Anang Fauzi <afauzx@gmail.com>
2026-07-20 22:05:27 +0700

    fix: gnome extension dispose label check

--- gnome/govubar@afx.com/extension.js
+++ gnome/govubar@afx.com/extension.js
@@ -6,77 +6,87 @@ import Gio from 'gi://Gio';
 
 import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 const Indicator = GObject.registerClass(
-class Indicator extends PanelMenu.Button {
-    _init() {
-        super._init(0.0, _('GoVUBar'));
+    class Indicator extends PanelMenu.Button {
+        _init() {
+            super._init(0.0, _('GoVUBar'));
 
-        this.label = new St.Label({
-            style_class: 'govubar-panel-label',
-            text: "○○○○○",
-            y_expand: true,
-            y_align: Clutter.ActorAlign.CENTER
-        });
+            this._destroyed = false;
+            this._subprocess = null;
 
-        this.add_child(this.label);
-
-        this._startSubprocess();
-    }
-
-    _startSubprocess() {
-        const script = `govubar`;
-
-        try {
-            this._subprocess = Gio.Subprocess.new(
-                ['bash', '-c', script],
-                Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.HAVE_STDERR
-            );
-
-            const stdoutStream = new Gio.DataInputStream({
-                base_stream: this._subprocess.get_stdout_pipe(),
-                close_base_stream: true,
+            this.label = new St.Label({
+                style_class: 'govubar-panel-label',
+                text: "○○○○○",
+                y_expand: true,
+                y_align: Clutter.ActorAlign.CENTER
             });
 
-            this._readOutput(stdoutStream);
-        } catch (e) {
-            logError(e);
+            this.add_child(this.label);
+
+            this._startSubprocess();
         }
-    }
 
-    _readOutput(stdout) {
-        if (!this.label || this.label.isDestroyed()) return;
-
-        stdout.read_line_async(GLib.PRIORITY_LOW, null, (stream, result) => {
+        _startSubprocess() {
             try {
-                const [line] = stream.read_line_finish_utf8(result);
+                this._subprocess = Gio.Subprocess.new(
+                    ['bash', '-c', 'govubar'],
+                    Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.HAVE_STDERR
+                );
 
-                if (line !== null) {
-                    this.label.text = line;
-                    this._readOutput(stdout);
-                }
+                this._stdout = new Gio.DataInputStream({
+                    base_stream: this._subprocess.get_stdout_pipe(),
+                    close_base_stream: true,
+                });
+
+                this._readOutput();
             } catch (e) {
-                logError(e);
+                logError(e, 'Failed to start govubar');
             }
-        });
-    }
-
-    vfunc_dispose() {
-        super.vfunc_dispose();
-
-        if (this._subprocess) {
-            this._subprocess.force_exit();
-            this._subprocess = null;
         }
 
-        if (this.label) {
-            this.label.destroy();
+        _readOutput() {
+            if (this._destroyed || !this._stdout)
+                return;
+
+            this._stdout.read_line_async(GLib.PRIORITY_DEFAULT, null, (stream, result) => {
+                    try {
+                        if (this._destroyed) return;
+
+                        const [line] = stream.read_line_finish_utf8(result);
+
+                        if (line === null) return;
+
+                        if (this.label) this.label.text = line;
+
+                        this._readOutput();
+                    } catch (e) {
+                        if (!this._destroyed) logError(e);
+                    }
+                }
+            );
+        }
+
+        destroy() {
+            this._destroyed = true;
+
+            if (this._subprocess) {
+                try {
+                    this._subprocess.force_exit();
+                } catch (e) {
+                    logError(e);
+                }
+
+                this._subprocess = null;
+            }
+
+            this._stdout = null;
             this.label = null;
+
+            super.destroy();
         }
-    }
-});
+    });
 
 export default class IndicatorExampleExtension extends Extension {
     enable() {
@@ -91,4 +101,3 @@ export default class IndicatorExampleExtension extends Extension {
         }
     }
 }
-
